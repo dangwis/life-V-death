e70341e875579f51c4b2fa2e16d6c8b324cb84ac
mike dobro <mdobro@umich.edu>
2016-11-07 23:08:48 -0500

    basic snake enemy and enemy base class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float movementSpeed;
+
+	protected GameObject[] players;
+
+	void Awake() {
+		players = GameObject.FindGameObjectsWithTag ("Life");
+	}
+
+	protected void MoveTowardsObject(GameObject P) {
+		if (P == null)
+			return;
+		float step = movementSpeed * Time.fixedDeltaTime;
+		transform.position =  Vector3.MoveTowards (transform.position, P.transform.position, step);
+		//print (P.transform.position);
+		transform.LookAt (P.transform.position);
+	}
+
+	protected GameObject FindClosestPlayer() {
+		float best = 0;
+		GameObject bestP = null;
+		foreach (GameObject p in players) {
+			float dist = Vector3.Distance (transform.position, p.transform.position);
+			if (dist > best) {
+				best = dist;
+				bestP = p;
+			}
+		}
+		return bestP;
+	}
+}
---
+++ Assets/Snake.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snake : Enemy {
+
+	GameObject targetPlayer = null;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("ChangeTarget", 0, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveTowardsObject (targetPlayer);
+	}
+
+	void ChangeTarget() {
+		targetPlayer = FindClosestPlayer ();
+	}
+}
