ca7e7255cb16654a1ffe9d90b7f997e6700a8acc
Eric Drenckpohl <esdrenck>
2016-11-10 14:41:56 -0500

    Missing bits

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+
+    public float start_time;
+    public int damage = 10;
+
+	// Use this for initialization
+	void Start () {
+        start_time = Time.time;
+
+        this.gameObject.GetComponent<Rigidbody>().velocity += (50 * this.gameObject.transform.forward);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnCollisionEnter(Collision coll) {
+        Destroy(this.gameObject);
+    }
+
+}
