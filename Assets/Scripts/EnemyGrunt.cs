5be6fa1a26e41199da5896fd18eb36da59e8942c
mike dobro <mdobro@umich.edu>
2016-11-23 12:10:13 -0500

    added grunt spawner

--- Assets/Scripts/EnemyGrunt.cs
+++ Assets/Scripts/EnemyGrunt.cs
@@ -3,28 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 
 // Basic chasing enemy
-public class Snake : Enemy {
+public class EnemyGrunt : MonoBehaviour {
+
+	public float health = 1f;
+	public float movementSpeed = 5f;
+	public float detectRange = 5f;
 
 	List<GameObject> players;
 	GameObject targetPlayer;
 
 	// Use this for initialization
-	/*void Start () {
+	void Start () {
 		InvokeRepeating ("ChangeTarget", 0f, 1f);
-	}*/
+	}
 	
 	// Update is called once per frame
-	/*protected override void Update () {
-		base.Update ();
+	void Update () {
 		GetComponent<Rigidbody> ().velocity = Vector3.zero;
 		if (targetPlayer == null)
 			ChangeTarget ();
-		MoveTowardsObject (targetPlayer);
+		Enemy.MoveTowardsObject (this.gameObject, targetPlayer, movementSpeed);
 		//snake has no attack, just moves toward play to do dmg
-	}*/
+	}
 
-	/*void ChangeTarget() {
-	//	players = DetectPlayers ();
-		targetPlayer = FindClosestPlayer (players);
-	}*/
+	void ChangeTarget() {
+		players = Enemy.DetectPlayers (this.gameObject, detectRange);
+		targetPlayer = Enemy.FindClosestPlayer (this.gameObject, players);
+	}
 }
---
+++ Assets/Scripts/GruntSpawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GruntSpawn : MonoBehaviour {
+	
+	public GameObject gruntPrefab;
+	public float maxSpawnTime = 5f;
+
+
+	private float spawnTimer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnTimer += Time.fixedDeltaTime;
+		if (spawnTimer > maxSpawnTime) {
+			SpawnEnemy ();
+			spawnTimer = 0f;
+		}
+	}
+
+	void SpawnEnemy() {
+		GameObject grunt = Instantiate (gruntPrefab, transform.position, transform.rotation) as GameObject;
+	}
+}
