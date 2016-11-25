80e51b311339481066d9163c2cc33a49fd116bd8
Daniel Engwis <dangwis@umich.edu>
2016-11-25 14:58:11 -0500

    Basic Destructable Barrel. I can't test this well because I have no controller at home.

---
+++ Assets/Scripts/Barrel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour {
+
+    public int healthRegen;
+    public float percentChance;
+    public GameObject healthPowerPrefab;
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject go = coll.gameObject;
+        if(go.tag == "Arrow" || go.tag == "Sword" || go.tag == "Hammer")
+        {
+            if (Random.value > percentChance)
+            {
+                GameObject hp = Instantiate(healthPowerPrefab);
+                hp.transform.position = transform.position;
+                hp.GetComponent<HealthPowerup>().healthToRestore = healthRegen;
+            }
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
