405cb4f619d0c3ab1da4432d736bfe5cf2f63e3a
Eric Drenckpohl <esdrenck>
2016-11-29 13:28:01 -0500

    New damage trap with cooldown

--- Assets/Scripts/DamageTrap.cs
+++ Assets/Scripts/DamageTrap.cs
@@ -5,22 +5,26 @@ public class DamageTrap : MonoBehaviour {
 
     public int damageDealt;
     Animation animate;
-    public bool armed, showing;
+    public bool armed = true, showing = false;
+    public ParticleSystem[] flame;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         animate = GetComponent<Animation>();
-        animate.Play("Anim_TrapNeedle_Hide");
+        //animate.Play("Anim_TrapNeedle_Hide");
         armed = true;
         showing = false;
-	}
+        foreach (ParticleSystem fire in flame) {
+            fire.Stop();
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
-    void OnTriggerEnter(Collider coll)
+    void OnTriggerStay(Collider coll)
     {
         GameObject go = coll.gameObject;
         if(go.tag == "Life" && armed == true)
@@ -29,8 +33,18 @@ public class DamageTrap : MonoBehaviour {
             go.GetComponent<LifePlayer>().state = 2;
             showing = true;
             this.gameObject.transform.Find("Trap_Needle").gameObject.layer = 0;
-            animate.Play("Anim_TrapNeedle_Show");
+            //animate.Play("Anim_TrapNeedle_Show");
+            foreach (ParticleSystem fire in flame) {
+                fire.Play();
+            }
+
+            armed = false;
+            Invoke("SetArmed", 1f);
         }
+    }
+
+    void SetArmed() {
+        armed = true;
     }
 
     void OnTriggerExit(Collider coll)
@@ -38,7 +52,10 @@ public class DamageTrap : MonoBehaviour {
         GameObject go = coll.gameObject;
         if (go.tag == "Life" && showing)
         {
-            animate.Play("Anim_TrapNeedle_Hide");
+            //animate.Play("Anim_TrapNeedle_Hide");
+            foreach (ParticleSystem fire in flame) {
+                fire.Stop();
+            }
             showing = false;
         }
     }
